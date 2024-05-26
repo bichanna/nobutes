6ddcaef014d3991b0dccd32c1e0c5f48722f5be7
bichanna <nobu.bichanna@gmail.com>
2024-05-26 13:11:38 -0400

    Remove router

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,6 @@
 import { render } from 'solid-js/web';
-import { Router, Route } from '@solidjs/router';
 
 import './index.css';
 import App from './App';
 
-render(() => (
-  <Router>
-    <Route path='/' component={App} />
-  </Router>
-), document.getElementById('root'));
+render(() => (<App/>), document.getElementById('root'));
