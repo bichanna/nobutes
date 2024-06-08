a0fcb5111ae0b9159f0af917a7a53b9f81c6634d
bichanna <nobu.bichanna@gmail.com>
2024-06-08 18:53:39 -0400

    Create add new note page

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,14 @@ import { Router, Route } from '@solidjs/router';
 import './index.css';
 import Notes from './pages/Notes';
 import Note from './pages/Note';
+import NewNote from './pages/NewNote';
 
 function App () {
   return (
     <Router>
       <Route path="/" component={Notes} />
       <Route path="/note/:id" component={Note} />
+      <Route path="/new" component={NewNote} />
     </Router>
   );
 }
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -45,42 +45,58 @@ function Notes() {
   }
 
   return <>
-    <div class="m-5 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
-      <For each={notes()}>
-        {(note, _) => (
-          <A
-            onContextMenu={(event) => { openModal(event); setSelectedNote(note); }}
-            href={"/note/" + note.id}
-            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{note.title}</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400" innerHTML={truncateNoteBody(note.body)}></p>
-          </A>
-        )}
-      </For>
-    </div>
+    <div class="m-5">
+      <h1 class="mb-3 text-4xl font-extrabold">No(bu)tes</h1>
+      
+      <div class="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
+        <A
+          href={"/new"}
+          class="items-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <div class="content-center relative w-12 h-12">
+            <svg class="w-full h-full text-gray-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+          </div>
+        </A>
+        <For each={notes()}>
+          {(note, _) => (
+            <A
+              onContextMenu={(event) => { openModal(event); setSelectedNote(note); }}
+              href={"/note/" + note.id}
+              class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{note.title}</h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400" innerHTML={truncateNoteBody(note.body)}></p>
+            </A>
+          )}
+        </For>
+      </div>
 
-    <div
-        class={`fixed z-10 inset-0 overflow-y-auto ${
-          isModalOpen() ? 'block' : 'hidden'
-        }`}
-    >
-      <div class="flex items-center justify-center min-h-screen">
-        {/* Modal content */}
-        <div class="bg-white rounded-lg shadow-lg p-6">
-          <h2 class="text-lg font-bold mb-4">Remove "{selectedNote().title}"?</h2>
-          <div class="flex justify-end">
-            <button
-              class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
-              onClick={handleConfirm}
-            >
-              Confirm
-            </button>
-            <button
-              class="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
-              onClick={() => setIsModalOpen(false)}
-            >
-              Cancel
-            </button>
+      <div
+          class={`fixed z-10 inset-0 overflow-y-auto ${
+            isModalOpen() ? 'block' : 'hidden'
+          }`}
+      >
+        <div class="flex items-center justify-center min-h-screen">
+          {/* Modal content */}
+          <div class="bg-white rounded-lg shadow-lg p-6">
+            <h2 class="text-lg font-bold mb-4">Remove "{selectedNote().title}"?</h2>
+            <div class="flex justify-end">
+              <button
+                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+                onClick={handleConfirm}
+              >
+                Confirm
+              </button>
+              <button
+                class="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setIsModalOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       </div>
